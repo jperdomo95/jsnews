4534528e6cf49d797e05d51f160a94beb91367e8
julio.perdomo <jperdomocorrea@gmail.com>
2022-05-27 09:59:02 -0400

    added useCallback to show and hideLoading

--- src/context/LoadingContext.tsx
+++ src/context/LoadingContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useCallback, useState } from 'react'
 
 export interface LoadingContextProps {
     isLoading: boolean;
@@ -11,13 +11,13 @@ export const LoadingContext = createContext( {} as LoadingContextProps )
 export const LoadingProvider = ({ children }: any ) => {
   const [isLoading, setIsLoading] = useState(false)
   
-  const showLoading = () => {
+  const showLoading = useCallback(() => {
     setIsLoading(true)
-  }
+  }, [])
 
-  const hideLoading = () => {
+  const hideLoading = useCallback(() => {
     setIsLoading(false)
-  }
+  }, [])
 
   return (
     <LoadingContext.Provider value={{
